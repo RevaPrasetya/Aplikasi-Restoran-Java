4083d64af0d3ac0d44752067d33dd213d9cf5598
revaprasetya <revabro3@gmail.com>
2023-04-17 10:38:50 +0800

    Change to Netbeans Project
    Changing from Intellij to Netbeans Project because the trial license to Intellij were already expired.

---
+++ src/gorestaurantbyreva/GoRestaurantByReva.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gorestaurantbyreva;
+
+/**
+ *
+ * @author Asus
+ */
+public class GoRestaurantByReva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
